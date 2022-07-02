c53609bb62913b0ac2f79929d4a4b9e5a0863300
srushti1hub <srushtirtv18@gmail.com>
2022-07-02 20:08:30 +0530

    set up for deployment

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const request = require('request')
 const path = require('path')
 const hbs = require('hbs')
 const { response } = require('express')
+
 const app = express()
+const port = process.env.PORT || 8000 
 
 const publicPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../views')
@@ -21,7 +23,6 @@ app.get('*',(req,res) => {
     res.send('ERROR 404')
 })
 
-app.listen(8080,() => {
-    console.log('Server started on port 8080')
-    console.log('Open => http://localhost:8080')
+app.listen(port,() => {
+    console.log('Server started on port '+port)
 })
